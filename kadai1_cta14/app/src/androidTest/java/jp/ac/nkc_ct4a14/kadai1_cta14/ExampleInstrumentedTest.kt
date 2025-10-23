86b3216d9aa43dfe94687b08b45d8ca0929a5ce7
Yuma Satake <satake.satake.y@gmail.com>
2025-10-23 12:29:58 +0900

    Initialize Gradle wrapper and project structure for kadai1_cta14
    - Added gradle-wrapper.properties to configure Gradle distribution.
    - Created gradlew and gradlew.bat scripts for Unix and Windows environments.
    - Set up settings.gradle.kts for dependency management and project configuration.

---
+++ kadai1_cta14/app/src/androidTest/java/jp/ac/nkc_ct4a14/kadai1_cta14/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package jp.ac.nkc_ct4a14.kadai1_cta14
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("jp.ac.nkc_ct4a14.kadai1_cta14", appContext.packageName)
+    }
+}
